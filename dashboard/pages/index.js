5b63505a1856f81078782fbf1b9ae77abb41dda2
Moreira4568 <carlosmoreira4568@hotmail.com>
2021-11-08 13:20:31 +0000

    Mockups update

--- dashboard/pages/index.js
+++ dashboard/pages/index.js
@@ -1,10 +1,10 @@
-import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import DesignerMan from './components/designer-man'
 import Navbar from './components/Layout/navbar'
 import Profile from './components/profile'
 import Menu from './components/menu'
+import Head from 'next/head'
 
 export default function Home() {
   return (
