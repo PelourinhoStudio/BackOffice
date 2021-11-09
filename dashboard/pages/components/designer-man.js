17cc7ec9b99578c11396cba20dd621ba98c0360f
paulosousa14 <64994606+paulosousa14@users.noreply.github.com>
2021-11-09 11:30:49 +0000

    date config

--- dashboard/pages/components/designer-man.js
+++ dashboard/pages/components/designer-man.js
@@ -1,76 +1,77 @@
 import React, { useEffect, useState } from "react"
 import api from '../../services/api'
 import Image from "next/dist/client/image";
+import moment from "moment";
+import "moment/locale/pt";
 
 export default function DesignerMan() {
 
     const [users, setUsers] = useState([]);
 
-
-  
-
     useEffect(() => {
         api
-          .get("/users")
-          .then((response) => setUsers(response.data))
-          .catch((err) => {
-            console.error("ops! ocorreu um erro" + err);
-          });
-      }, []);
+            .get("/users")
+            .then((response) => setUsers(response.data))
+            .catch((err) => {
+                console.error("ops! ocorreu um erro" + err);
+            });
+    }, []);
 
 
-    return(
-<>
+    return (
+        <>
 
-<div className="text-center m-3 rounded-main bg-white">
-        <table className="table table-striped table-hover">
-                <thead>
-                    <tr>
-                        <th>Nome</th>						
-                        <th>Ultima vez Online</th>
-                        <th>Estatuto</th>
-                        <th>Estado</th>
-                        <th>Email</th>
-                        <th>Ações</th>
-                    </tr>
-                </thead>
-                <tbody>
-                   
+            <div className="text-center m-3 rounded-main bg-white">
+                <table className="table table-striped table-hover">
+                    <thead>
+                        <tr>
+                            <th>Nome</th>
+                            <th>Ultima vez Online</th>
+                            <th>Estatuto</th>
+                            <th>Estado</th>
+                            <th>Email</th>
+                            <th>Ações</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
 
                         {
-        users.map((user) => {
-           
-          return (
-         <tr key={user.email}>
-         <td><a href="#"><img src={user.avatar} className="avatar" alt="Avatar"/></a></td>
-         <td>{user.lastOnline}</td>                        
-         <td>
-            {user.userType}
-         </td>
-         <td>
-            {
-                String(user.state).toUpperCase() == 'ATIVO' ? <><span className="status text-success">•</span> Ativo</> : <></>
-            }
-            {
-                String(user.state).toUpperCase() == 'INATIVO' ? <><span className="status text-danger">•</span> Inativo</> : <></>
-            }
-                        {
-                String(user.state).toUpperCase()== 'SUSPENSO' ? <><span className="status text-warning">•</span> Suspenso</> : <></>
-            }
-        </td>
-         <td>{user.email}</td>
-         <td>
-             <a href="#" className="settings mr-2" title="" data-toggle="tooltip" data-original-title="settings mr-2"><i className="fas fa-cog text-danger"></i></a>
-             <a href="#" className="delete" title="" data-toggle="tooltip" data-original-title="Delete"><i className="fas fa-times-circle text-primary"></i></a>
-         </td>
-     </tr>
-         )
-        })
-      }
-                </tbody>
-            </table>
-        </div>
-</>
-        
+                            users.map((user) => {
+
+                                return (
+                                    <tr key={user.email}>
+                                        <td><a href="#"><img src={user.avatar} className="avatar" alt="Avatar" /></a></td>
+                                        <td>
+                                            {'Online ' + moment(moment(user.lastOnline).format('DD-MM-YYYY'), "DDMMYYYY").fromNow()}
+                                        </td>
+                                        <td>
+                                            {String(user.userType).toUpperCase()}
+                                        </td>
+                                        <td>
+                                            {
+                                                String(user.state).toUpperCase() == 'ATIVO' ? <><span className="status text-success">•</span> Ativo</> : <></>
+                                            }
+                                            {
+                                                String(user.state).toUpperCase() == 'INATIVO' ? <><span className="status text-danger">•</span> Inativo</> : <></>
+                                            }
+                                            {
+                                                String(user.state).toUpperCase() == 'SUSPENSO' ? <><span className="status text-warning">•</span> Suspenso</> : <></>
+                                            }
+                                        </td>
+                                        <td>{user.email}</td>
+                                        <td>
+                                            <a href="#" className="settings mr-2" title="" data-toggle="tooltip" data-original-title="settings mr-2"><i className="fas fa-cog text-danger"></i></a>
+                                            <a href="#" className="delete" title="" data-toggle="tooltip" data-original-title="Delete"><i className="fas fa-times-circle text-primary"></i></a>
+                                        </td>
+                                    </tr>
+                                )
+                            })
+                        }
+                    </tbody>
+                </table>
+            </div>
+        </>
+
     );
-  }
+}
--- dashboard/pages/index.js
+++ dashboard/pages/index.js
@@ -8,29 +8,29 @@ import Head from 'next/head'
 
 export default function Home() {
 
-  return (
-<>
-<Head>
-   <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossOrigin="anonymous"/>
-</Head>
+   return (
+      <>
+         <Head>
+            <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossOrigin="anonymous" />
+         </Head>
 
-<div className="main-container">
-   <div className="m-auto max-width">
-      <Navbar/>
-      <div className="rounded-main main-contents d-none mt-2 d-md-block">
-         <DesignerMan/>
-      </div>
-      <div className="w-100 roudend mb-3">
-         <div className="m-auto m-md-0" style={{width: "317px"}}>
-         <Profile/>
-      </div>
-   </div>
-   <div className="bg-secondary w-100 rounded-main p-3">
-   <Menu></Menu>
-</div>
-</div>
-</div>
+         <div className="main-container">
+            <div className="m-auto max-width">
+               <Navbar />
+               <div className="rounded-main main-contents d-none mt-2 d-md-block">
+                  <DesignerMan />
+               </div>
+               <div className="w-100 roudend mb-3">
+                  <div className="m-auto m-md-0" style={{ width: "317px" }}>
+                     <Profile />
+                  </div>
+               </div>
+               <div className="bg-secondary w-100 rounded-main p-3">
+                  <Menu></Menu>
+               </div>
+            </div>
+         </div>
 
-</>
-  )
+      </>
+   )
 }
